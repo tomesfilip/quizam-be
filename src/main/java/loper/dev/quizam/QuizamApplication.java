a9f967a55dd5878c762a22be6e5c07d1a25f9f5c
Filip Tomeš <fitome@ttu.ee>
2023-04-15 09:48:21 +0300

    connection to mongodb

---
+++ src/main/java/loper/dev/quizam/QuizamApplication.java
@@ -0,0 +1,13 @@
+package loper.dev.quizam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loper/dev/quizam/QuizamApplicationTests.java
@@ -0,0 +1,13 @@
+package loper.dev.quizam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
